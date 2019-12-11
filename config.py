73fbf58dc3bfe06b66b06295721dc8788536f7c5
thomasguy <tom@SportThailand.localdomain>
2019-12-11 22:47:16 +0700

    update wsl path

--- config.py
+++ config.py
@@ -7,3 +7,15 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "sqlite:////c/data/sqlite/db/bfx_serverDB.db" or None
+
+
+# # open a WSL terminal.
+
+# # Create and modify the new WSL configuration file:
+# sudo nano /etc/wsl.conf
+
+# # To remove /mnt/c/ from wsl windows paths, so all windows paths start /c/:
+# # make wsl.conf look like this:-
+# [automount]
+# root = /
+# options = "metadata"
